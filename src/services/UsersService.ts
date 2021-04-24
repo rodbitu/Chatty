0c34e289c4bddbbf231989fc7fbd6209385b6479
Rodrigo Bitu <digomoraes0@gmail.com>
2021-04-24 01:05:15 -0300

    Commiting project NLW #5

---
+++ src/services/UsersService.ts
@@ -0,0 +1,43 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+import { User } from "../entities/User";
+
+class UsersService {
+    private usersRepository: Repository<User>
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string) {
+        //Verificar se o user existe
+
+        const userExists = await this.usersRepository.findOne({ 
+            email,
+        })
+
+         //Se existir, retornar user
+        if(userExists) {
+            return userExists;
+        }
+
+        
+        const user = this.usersRepository.create({
+            email,
+        })
+
+        await this.usersRepository.save(user);
+       
+        //Se não existir, salvar no DB
+        return user;
+    }
+
+    async findByEmail(email: string) {
+        const user = await this.usersRepository.findOne({ email });
+      
+        return user;
+    }
+
+}
+
+export { UsersService };
